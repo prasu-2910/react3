18c75a57548e983736bed6347f15fa0ee57f0ea0
prasu-2910 <prasuaruna29@gmail.com>
2025-12-17 15:19:41 +0530

    third commit

--- Q3/code/src/App.jsx
+++ Q3/code/src/App.jsx
@@ -1,12 +1,29 @@
-import Counter from "./Counter";
-
-function App() {
-  return (
+import Messagecard from "./MessageCard";
+function App(){
+  return(
     <div>
-      <h2>Counter App</h2>
-      <Counter />
-    </div>
-  );
-}
+      <h2>Message cards</h2>
 
+      <Messagecard
+      title="welcome"
+      message="Welcome to React Application"
+      />
+      <Messagecard
+      title="Success"
+      message="Your data has  been added successfully"
+      />
+      <Messagecard
+      title="warning"
+      message="Please check the entered details"
+      />
+      <Messagecard
+      title="info"
+      message="This is a reusable component example"
+      />
+
+
+    </div>
+  )
+
+}
 export default App;
---
+++ Q3/code/src/MessageCard.jsx
@@ -0,0 +1,14 @@
+
+function Messagecard ({title,message}){
+  return(
+     <div style={{
+      border: "1px solid #ccc",
+      padding: "12px",
+      margin: "10px",
+      borderRadius: "6px"
+    }}><h3>{title}</h3>
+      <p>{message}</p></div>
+  );
+
+};
+export default Messagecard;
